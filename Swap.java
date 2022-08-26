927f6f82788473a659f0ada708cc76dc773e06ef
Hasnayn <91376617+Hasnayn78@users.noreply.github.com>
2022-08-25 19:20:35 -0700

    Add files via upload
     A bunch of java  solved problems for beginners.

---
+++ Swap.java
@@ -0,0 +1,23 @@
+public class Swap {
+
+
+    public static void main(String[] args) {
+        int numA=12;
+        int numB=14;
+        int temp;
+        System.out.println("Before swapping");
+
+        System.out.println(numA);
+        System.out.println(numB);
+
+        temp = numA;
+        numA=numB;
+        numB=temp;
+
+        System.out.println("After Swapping ");
+
+        System.out.println(numA);
+        System.out.println(numB);
+
+    }
+}
